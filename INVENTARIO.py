64a52c2e3bd4911022029e87934e5187c5dc2ae7
luisseoanes <65507700+luisseoanes@users.noreply.github.com>
2024-09-15 11:34:48 -0500

    Update INVENTARIO.py

--- INVENTARIO.py
+++ INVENTARIO.py
@@ -1,50 +1,63 @@
 import pandas as pd
-from PRODUCTO import PRODUCTO
+import os
+import PRODUCTO as PRODUCTO
 
 class INVENTARIO:
-    def __init__(self):
-        self.inventario = pd.read_csv('Datos/bodega.csv')
-    
-    def agregarProducto(self, producto):
-        """Agregar un nuevo producto al inventario."""
-        if isinstance(producto, PRODUCTO):
-            nuevo_producto = pd.DataFrame([producto.to_dict()])
-            self.inventario = pd.concat([self.inventario, nuevo_producto], ignore_index=True)
-            self.inventario.to_csv('bodega.csv', index=False)  # Guardar cambios en el archivo CSV
-            print("Producto agregado con éxito.")
-        else:
-            print("Error: El objeto no es una instancia de Producto.")
-    
-    def eliminarProducto(self, idProducto):
+    def __init__(self, nombreTienda):
+        self.nombreTienda = nombreTienda
+        self.ruta_bodega = os.path.join('Tiendas', self.nombreTienda, 'bodega.csv')
+
+        self.inventario = pd.read_csv(self.ruta_bodega)
+
+    def agregarProducto(self):
+        
+        # Solicitar información del producto al usuario
+        id_producto = input("Ingrese el ID del producto: ")
+        nombre = input("Ingrese el nombre del producto: ")
+        descripcion = input("Ingrese la descripción del producto: ")
+        precio = float(input("Ingrese el precio del producto: "))
+        cantidad_stock = int(input("Ingrese la cantidad en stock: "))
+        proveedor = input("Ingrese el nombre del proveedor: ")
+        fechaIngreso = input("Ingrese la fecha de ingreso: ")
+
+
+        # Crear un diccionario con la información del nuevo producto
+        nuevo_producto = {
+            'ID': id_producto,
+            'Nombre del Producto': nombre,
+            'Descripcion': descripcion,
+            'Precio por Unidad': precio,
+            'Cantidad en Stock': cantidad_stock,
+            'Proveedor' : proveedor,
+            'Fecha de Ingreso': fechaIngreso
+        }
+
+        # Convertir el diccionario a un DataFrame
+        nuevo_producto_df = pd.DataFrame([nuevo_producto])
+        # Agregar el Dataframe al archivo
+        self.inventario = pd.read_csv(self.ruta_bodega)
+        self.inventario = pd.concat([self.inventario, nuevo_producto_df], ignore_index=True)
+        self.inventario.to_csv(self.ruta_bodega, index=False)
+        
+        print("Producto agregado con éxito.")
+
+    def eliminarProducto(self):
         """Eliminar un producto del inventario por su ID."""
-        if idProducto in self.inventario['ID'].values:
-            self.inventario = self.inventario[self.inventario['ID'] != idProducto]
-            self.inventario.to_csv('bodega.csv', index=False)  # Guardar cambios en el archivo CSV
+        idProducto = input("Ingrese el ID del producto que desea eliminar: ")
+
+        if self.inventario['ID'].astype(str).isin([str(idProducto)]).any():
+            self.inventario = self.inventario[self.inventario['ID'].astype(str) != str(idProducto)]
+            self.inventario.to_csv(self.ruta_bodega, index=False)  # Guardar cambios en el archivo CSV
             print(f"Producto con ID {idProducto} eliminado.")
         else:
             print(f"Producto con ID {idProducto} no encontrado.")
-    
+
     def buscarProducto(self, idProducto):
-        """Buscar un producto por su ID."""
-        producto = self.inventario[self.inventario['ID'] == idProducto]
-        if not producto.empty:
-            # Convertir DataFrame a instancia de Producto
-            prod_dict = producto.iloc[0].to_dict()
-            return PRODUCTO(prod_dict['ID'], prod_dict['Nombre'], prod_dict['Descripción'], 
-                            prod_dict['Precio'], prod_dict['Cantidad en Stock'])
-        else:
-            print(f"Producto con ID {idProducto} no encontrado.")
-            return None
-    
+
+        self.inventario["ID"] = self.inventario['ID'].astype(str)
+        producto = self.inventario.loc[self.inventario['ID'] == idProducto]
+        print(producto)
+
     def listarProductos(self):
         """Listar todos los productos del inventario."""
         return self.inventario
-    
-    def actualizarInventario(self, idProducto, nuevaCantidad):
-        """Actualizar la cantidad en stock de un producto dado su ID."""
-        if idProducto in self.inventario['ID'].values:
-            self.inventario.loc[self.inventario['ID'] == idProducto, 'Cantidad en Stock'] = nuevaCantidad
-            self.inventario.to_csv('bodega.csv', index=False)  # Guardar cambios en el archivo CSV
-            print(f"Cantidad del producto con ID {idProducto} actualizada a {nuevaCantidad}.")
-        else:
-            print(f"Producto con ID {idProducto} no encontrado.")
